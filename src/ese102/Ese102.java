95afdd4756cfa8cdf312c186d236f4ec9bf19328
Francesco Trotta <frtrotta@users.noreply.github.com>
2015-03-11 10:12:34 +0100

    Implementazione con ovveriding di toString e equals

---
+++ src/ese102/Ese102.java
@@ -0,0 +1,26 @@
+package ese102;
+
+import complexnumber.*;
+
+public class Ese102 {
+
+	public static void main(String[] args) {
+		ComplexNumber.setInitRectangular(0, 0);
+		ComplexNumber c1 = new ComplexNumber();
+		ComplexNumber.setInitRectangular(1, 1);
+		ComplexNumber c2 = new ComplexNumber();
+		System.out.println("c1: " + c1);
+		System.out.println("c2: " + c2);
+		System.out.println("Addiction: " + c1.add(c2));
+		ComplexNumber r = c1.add(c2);
+		r.setStringFormat(ComplexNumber.StringFormat.POLAR);
+		System.out.println("Addiction (polar): " + r);
+		System.out.println("Subtraction: " + c1.sub(c2));
+		/* Domanda: come posso controllare il formato della
+		 * rappresentazione a stringa del risultato dell'operazione, senza dover assegnare
+		 * il risultato ad una variabile?
+		 */
+		System.out.println("Subtraction (polar): " + c1.sub(c2));
+	}
+
+}
